4541582358cb33d84eb3271bb1cd648be331edf3
Eriasuitor <funtree9@icloud.com>
2018-09-06 20:33:52 +0800

    alomost success

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let rp = require('request-promise').defaults({ jar })
 const fs = require('fs')
 
 async function start() {
-    let uuid, redirect_uri, wxuin, wxsid, webwx_data_ticket
+    let uuid, redirect_uri, wxuin, wxsid, webwx_data_ticket, syncKey, i = 1, webwxuvid
     await rp.get({
         url: `https://login.weixin.qq.com /jslogin?appid=wx782c26e4c19acffb&redirect_uri=https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxnewloginpage&fun=new& amp;lang=zh_CN&_=${new Date().getTime()}`
     }).then(body => {
+        fs.writeFileSync(i + '.json', body)
+        fs.writeFileSync(i++ + 'Jar.json', JSON.stringify(jar))
         uuid = parse(body)['window.QRLogin.uuid']
     }).then(() => {
         rp.get(`https://login.weixin.qq.com/qrcode/${uuid}?t=webwx`).pipe(fs.createWriteStream('./qrcode.png'))
@@ -38,15 +40,14 @@ async function start() {
                 })
             }
         }
+        fs.writeFileSync(i++ + 'Jar.json', JSON.stringify(jar));
+        ({ wxuin, webwxuvid, webwx_data_ticket } = parse(jar.getCookieString('https://login.weixin.qq.com')));
         return rp.get({
-            url: redirect_uri,
-            headers: {
-                'Upgrade-Insecure-Requests': 1,
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
-            }
+            url: redirect_uri + '&fun=new',
         })
     }).then(body => {
+        fs.writeFileSync(i + '.txt', body)
+        fs.writeFileSync(i++ + 'Jar.json', JSON.stringify(jar));
         ({ wxuin, wxsid, webwx_data_ticket } = parse(jar.getCookieString(redirect_uri)));
         return rp.post({
             url: `https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxinit?r=${new Date().getTime()}`,
@@ -64,18 +65,31 @@ async function start() {
         })
 
     }).then(body => {
-        fs.writeFileSync('first.json', body)
+        fs.writeFileSync(i + '.json', body)
+        fs.writeFileSync(i++ + 'Jar.json', JSON.stringify(jar))
+        // fs.writeFileSync('first.json', body)
+        syncKey = JSON.parse(body).SyncKey.List.map(sk => `${sk.Key}_${sk.Val}`).join('|')
         // console.log(JSON.parse(body))
-        fs.writeFileSync('jar.txt', JSON.stringify(jar))
+        fs.writeFileSync('jar2.json', JSON.stringify(jar))
         console.log(wxuin, wxsid, webwx_data_ticket)
-        rp.get({
+        return rp.get({
             url: `https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxgetcontact?type=ex&&r=${new Date().getTime()}`,
             headers: {
                 'Cookie': `wxuin=${wxuin}; wxsid=${wxsid}; webwx_data_ticket=${webwx_data_ticket}`
             }
-        }).then(body => {
-            console.log(body)
         })
+    }).then(body => {
+        fs.writeFileSync(i + '.json', body)
+        fs.writeFileSync(i++ + 'Jar.json', JSON.stringify(jar))
+        return rp.get({
+            url: `https://webpush.wx2.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=${new Date().getTime()}&sid=${wxsid}&uin=${wxuin}&deviceid=e45382650334865&synckey=${syncKey}`
+        })
+    }).then(body => {
+        fs.writeFileSync(i + '.json', body)
+        fs.writeFileSync(i++ + 'Jar.json', JSON.stringify(jar))
+        // rp.post({
+        //     url: `https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?lang=zh_CN&pass_ticket=`,
+        // })
     })
 }
 
@@ -89,18 +103,19 @@ function parse(source) {
     return retJson
 }
 
-// start()
+start()
 
-let jar = JSON.parse(fs.readFileSync('jar.txt').toString())
+// let jar = JSON.parse(fs.readFileSync('jar.txt').toString())
 
-let { wxuin, wxsid, webwx_data_ticket } = parse(jar.getCookieString(redirect_uri))
+// let { wxuin, wxsid, webwx_data_ticket } = parse(jar.getCookieString(redirect_uri))
 
-rp.post({
-    url: `https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=${wxsid}&r=${new Date().getTime()}`,
-    body: JSON.stringify({}),
-    headers: {
-        'Cookie': `wxuin=1134794182; wxsid=ErMKFxi0oyzG7OJq; webwx_data_ticket=gSei+kZ5AvnYSHXpVRVAcw2J`
-    }
-}).then(body => {
-    console.log(body)
-})
+// rp.post({
+//     url: `https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=${wxsid}&r=${new Date().getTime()}`,
+//     url: `https://webpush.wx2.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=${new Date().getTime()}&skey=&sid=${wxsid}&uin=${wxuin}&deviceid=e45382650334865&synckey=1_679319969%7C2_679320119%7C3_679320045%7C11_679319441%7C201_1536229775%7C1000_1536222603%7C1001_1536222675&_=1536229586841`
+//     body: JSON.stringify({}),
+//     headers: {
+//         'Cookie': `wxuin=1134794182; wxsid=ErMKFxi0oyzG7OJq; webwx_data_ticket=gSei+kZ5AvnYSHXpVRVAcw2J`
+//     }
+// }).then(body => {
+//     console.log(body)
+// })
